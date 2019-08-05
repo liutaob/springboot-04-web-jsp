fbd104d14ed6ffad4b9bfaa6779658dc59728cde
liut <244160321@qq.com>
2019-08-06 00:11:19 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.atguigu.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/abc")
+    public String abc(Model model, HttpServletRequest request) {
+        //获取上下文环境绝对路径
+        request.getSession().getServletContext().getRealPath("/");
+        model.addAttribute("hello", "你好");
+        return "success";
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liut
+  Date: 2019/4/25
+  Time: 20:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+hello1111
+<a href="abc">跳转</a>
+</body>
+</html>
